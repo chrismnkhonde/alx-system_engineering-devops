b3b595a3dcade9f767868584e530219afe124094
christopher <chrismnkhonde@gmail.com>
2023-03-20 02:32:14 -0700

    2-assembler

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with using the printf function
+ * Ruturn: 0 (Success)
+  */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
